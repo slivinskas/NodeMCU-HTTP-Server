ec43e2b8aa906a56731b087f59a44eb3029a3521
SLIVINSKAS\slivi <slivinskasvytenis@gmail.com>
2018-07-15 22:18:07 +0300

    change Blink class file name added Blink calls

--- example/init.lua
+++ example/init.lua
@@ -1,6 +1,9 @@
-dofile("LED-blinking.lua")
-mainLedBlinking(1000)
+dofile ("Blink.lua")
+local timer = require 'timer'
+local mainLed =  Blink:new(4,500)
+mainLed:startBlink()
 dofile('wifiConnection.lua')
+mainLed:startBlink()
 -- Serving static files
 dofile('httpServer.lua')
 dofile('serverAPI.lua')
